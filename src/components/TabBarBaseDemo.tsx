3c3a5f75055025d8674a56ea324ffec4e7b89b49
kpmark <2585050765@qq.com>
2025-06-20 13:18:30 +0800

    feat: 引入任务栏
    - 使用tdesign-mobile-react UI库
    - 新增less配置
    - 初始化任务栏示例

---
+++ src/components/TabBarBaseDemo.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { Icon } from "tdesign-icons-react";
+import { TabBar, TabBarItem } from "tdesign-mobile-react";
+
+function TabBarBaseDemo() {
+  const list = [
+    { value: "label_1", icon: "home", ariaLabel: "首页" },
+    { value: "label_2", icon: "app", ariaLabel: "软件" },
+    { value: "label_3", icon: "chat", ariaLabel: "聊天" },
+  ];
+  const [value, setValue] = useState("label_1");
+
+  const change = (changeValue: string | number) => {
+    setValue(changeValue as string);
+    console.log("TabBar 值改变为：", changeValue);
+  };
+
+  useEffect(() => {
+    console.log("当前值：", value);
+  }, [value]);
+
+  return (
+    <div className="demo-tab-bar">
+      <TabBar value={value} onChange={change} theme="tag" split={false}>
+        {list.map((item, i) => (
+          <TabBarItem key={item.value || i} icon={<Icon name={item.icon} />} value={item.value} />
+        ))}
+      </TabBar>
+    </div>
+  );
+}
+
+export default TabBarBaseDemo;
