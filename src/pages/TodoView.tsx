d0f9c5bb111a9e78c682d9f8bfcfdb9abbabd8b9
Acidbarium <acidbarium@163.com>
2025-06-22 13:14:22 +0800

    refactor: 移除多余的 Nautilus 组件，合并功能至 TodoView
    原有 Nautilus 组件仅作为 TimerPanel 和 TodoPanel 的包装，层级多余。
    本次将其结构直接合并至 TodoView，优化组件结构，减少无效嵌套。

--- src/pages/TodoView.tsx
+++ src/pages/TodoView.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-import Nautilus from "../components/Nautilus";
+// import React from "react";
+// import Nautilus from "../components/Nautilus";
 
-const NautilusView: React.FC = () => {
+// const NautilusView: React.FC = () => {
+//   return (
+//     <div className="nautilus-view">
+//       <Nautilus />
+//     </div>
+//   );
+// };
+
+// export default NautilusView;
+
+import React from "react";
+import TimerPanel from "../components/TimerPanel";
+import TodoPanel from "../components/TodoPanel";
+import "../styles/components/Nautilus.css";
+
+const TodoView: React.FC = () => {
   return (
-    <div className="nautilus-view">
-      <Nautilus />
+    <div className="app-container">
+      <div className="app-content">
+        <div className="main-grid">
+          <TimerPanel />
+          <TodoPanel />
+        </div>
+      </div>
     </div>
   );
 };
 
-export default NautilusView;
-
+export default TodoView;
