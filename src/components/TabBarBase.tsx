288b28edd8b324e2505ac5c36a13701a98823a66
gyf <2528357407@qq.com>
2026-01-17 11:20:15 +0800

    style:优化底部栏图标

--- src/components/TabBarBase.tsx
+++ src/components/TabBarBase.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Icon } from "tdesign-icons-react";
+import {
+  CheckSquareOutlined,
+  CalendarOutlined,
+  RobotOutlined,
+  ProjectOutlined,
+} from "@ant-design/icons";
 import { TabBar, TabBarItem } from "tdesign-mobile-react";
 import "../styles/components/TabBarBase.css";
 
 function TabBarBase() {
   const list = [
-    { value: "/", icon: "home", ariaLabel: "Todo" },
-    { value: "/calendar", icon: "app", ariaLabel: "Calendar" },
-    { value: "/chat", icon: "chat", ariaLabel: "Chat" },
-    { value: "/myPlan", icon: "user", ariaLabel: "MyPlan" },
+    { value: "/", icon: <CheckSquareOutlined />, ariaLabel: "Todo" },
+    { value: "/calendar", icon: <CalendarOutlined />, ariaLabel: "Calendar" },
+    { value: "/chat", icon: <RobotOutlined />, ariaLabel: "Chat" },
+    { value: "/myPlan", icon: <ProjectOutlined />, ariaLabel: "MyPlan" },
   ];
 
   const navigate = useNavigate();
@@ -29,7 +34,11 @@ function TabBarBase() {
     <div className="demo-tab-bar">
       <TabBar value={value} onChange={change} theme="tag" split={false}>
         {list.map((item, i) => (
-          <TabBarItem key={item.value || i} icon={<Icon name={item.icon} />} value={item.value} />
+          <TabBarItem
+            key={item.value || i}
+            icon={item.icon}
+            value={item.value}
+          />
         ))}
       </TabBar>
     </div>
